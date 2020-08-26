2d5c8b0e28ffc73839bbe4df8ad763d7acd8e855
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-26 11:01:37 +0300

    Add separator function

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const createElement = (element, className) => {
-  const elm = document.createElement(element);
-  elm.className = className;
-  return elm;
-};
-
-const createElementWithText = (element, className, text) => {
-  const elm = document.createElement(element);
-  elm.className = className;
-  elm.innerHTML = text;
-  return elm;
-};
-
-const createImage = (src, alt, className) => {
-  const myImage = new Image();
-  myImage.src = src;
-  myImage.alt = alt;
-  myImage.className = className;
-  return myImage;
-};
-
 export {
   createElement,
   createElementWithText,
   createImage,
-};
+} from './lib/htmlElements';
+
+export { default as separate } from './lib/separator';
